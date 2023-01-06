f7ba4e9c4909a212ddf002c9a82c3b94d41c6627
kirill <johndoe@example.com>
2023-01-07 00:09:29 +0100

    add: frontend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,127 +1,144 @@
 import PostModel from '../models/post.js';
 
 export const getAll = async (req, res) => {
-    try {
-        const posts = await PostModel.find().populate('user').exec();
-        res.json(posts);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to get articles"
+  try {
+    const posts = await PostModel.find().populate('user').exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to get articles',
     });
-    }
+  }
 };
 
 export const getOne = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        PostModel.findByIdAndUpdate(
-            {
-                _id: postId,
-            },
-            {
-                $inc: { viewCount: 1},
-            },
-            {
-                returnDocument: 'after',
-            },
-            (err, doc) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).json({
-                        message: "Failed to return article"
-                });
-                };
-                if (!doc) {
-                    return res.satus(404).json({
-                        message: 'Article not found'
-                    })
-                }
-                res.json(doc);
+  try {
+    const postId = req.params.id;
+    PostModel.findByIdAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Failed to return article',
+          });
         }
-        );
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to get an article"
+        if (!doc) {
+          return res.satus(404).json({
+            message: 'Article not found',
+          });
+        }
+        res.json(doc);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to get an article',
     });
-    }
+  }
 };
 
 export const create = async (req, res) => {
-    try {
-        const doc = new PostModel({
-            title: req.body.title,
-            text: req.body.text,
-            imageUrl: req.body.imageUrl,
-            tags: req.body.tags,
-            user: req.userId,
-        });
-
-        const post = await doc.save();
-        res.json(post);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Post creation fail"
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
     });
-    }
+
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Post creation fail',
+    });
+  }
 };
 
 export const remove = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        PostModel.findOneAndDelete({
-            _id: postId,
-        },
-        (err, doc) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    message: "Failed to delete an article"
-                    });
-            }
-            if (!doc) {
-                console.log(err);
-                return res.status(500).json({
-                    message: "Article not found"
-                    });
-            }
-            res.json({
-                success: true,
-            })
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Failed to delete an article',
+          });
         }
-        );
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to get an article"
+        if (!doc) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Article not found',
+          });
+        }
+        res.json({
+          success: true,
+        });
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to get an article',
     });
-    }
+  }
 };
 
 export const update = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        await PostModel.updateOne(
-            {
-                _id: postId,
-            },
-            {
-                title: req.body.title,
-                text: req.body.text,
-                imageUrl: req.body.imageUrl,
-                user: req.userId,
-                tegs: req.body.tags,
-            }
-        );
-        res.json({
-            success: true,
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to update an article"
+  try {
+    const postId = req.params.id;
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tegs: req.body.tags,
+      },
+    );
+    res.json({
+      success: true,
     });
-    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to update an article',
+    });
+  }
+};
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to update an article',
+    });
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,40 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 import multer from 'multer';
-import {validationResult} from 'express-validator';
+import cors from 'cors';
+import { validationResult } from 'express-validator';
 
-import {loginValidation, registerValidation, postCreateValidation} from './validations.js';
-import {userController, postController} from './controllers/index.js';
-import {handleValidationErrors, checkAuth} from './utils/index.js';
+import { loginValidation, registerValidation, postCreateValidation } from './validations.js';
+import { userController, postController } from './controllers/index.js';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://kek:qhWZ4so6Ca6tpdmJ@cluster0.ftkilue.mongodb.net/blog?retryWrites=true&w=majority")
-    .then(()=> console.log('DB OK'))
-    .catch((err)=> console.log("DB error", err));
+mongoose
+  .connect(
+    'mongodb+srv://kek:qhWZ4so6Ca6tpdmJ@cluster0.ftkilue.mongodb.net/blog?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB OK'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (_, __, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (_, file, cb) => {
-        cb(null, file.originalname);
-    },
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
-    res.send("hello");
+  res.send('main page');
 });
 
 //user
@@ -38,21 +43,28 @@ app.post('/auth/register', registerValidation, handleValidationErrors, userContr
 app.get('/auth/me', checkAuth, userController.getMe);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`,
-    });
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 });
 
-//posts 
+//posts
 app.get('/posts', postController.getAll);
+app.get('/tags', postController.getLastTags);
 app.get('/posts/:id', postController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, postController.create);
 app.delete('/posts/:id', checkAuth, postController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, postController.update);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  postController.update,
+);
 
 app.listen(4444, (err) => {
-    if(err) {
-        return console.log(err)
-    }
-    console.log("Server OK");
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Server OK');
 });
